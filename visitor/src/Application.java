2b4caafdb79b9e225277c6b40139910fb3a91937
Kibeom Kwon <xtcc02332@naver.com>
2025-01-22 13:15:05 +0900

    visitor pattern

---
+++ visitor/src/Application.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+
+        List<Visitable> visitables = new ArrayList<>();
+
+        visitables.add(new VisitableA(1));
+        visitables.add(new VisitableA(2));
+        visitables.add(new VisitableA(3));
+        visitables.add(new VisitableA(4));
+        visitables.add(new VisitableA(5));
+
+        Visitor visitor = new VisitorA();
+
+        for (Visitable visitable : visitables) {
+            visitable.accept(visitor);
+        }
+
+        System.out.println(((VisitorA) visitor).getAgeSum());
+    }
+}
